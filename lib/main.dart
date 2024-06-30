7de293fb58a4921f299dfcbfbb76d452ff593854
Sumira <sumiragp@gmail.com>
2024-06-30 22:55:32 +0530

    vertical and horizontal scrollable listviews added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:lottery_app/screens/home/home.dart';
 
 void main() {
@@ -17,7 +19,106 @@ class Sandbox extends StatefulWidget {
 class _SandboxState extends State<Sandbox> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
+      body: Column(
+        children: <Widget>[
+          // const SizedBox(
+          //   height: 10,
+          // ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                child: Container(
+                  alignment: Alignment.center,
+                  constraints: BoxConstraints(
+                    maxWidth: screenWidth - 10,
+                    maxHeight: 200,
+                  ),
+                  color: Colors.cyan.shade400,
+                  child: const Text('ClipRRect'),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(8),
+              children: [
+                Container(
+                  height: 50,
+                  width: 100,
+                  color: Colors.red[600],
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  color: Colors.red[500],
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  color: Colors.red[400],
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  color: Colors.red[300],
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  color: Colors.red[200],
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  color: Colors.red[100],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+
+          Expanded(
+              child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: [
+              Container(
+                height: 150,
+                color: Colors.amber[600],
+                child: const Center(child: Text('Entry A')),
+              ),
+              Container(
+                height: 150,
+                color: Colors.amber[500],
+                child: const Center(child: Text('Entry B')),
+              ),
+              Container(
+                height: 150,
+                color: Colors.amber[400],
+                child: const Center(child: Text('Entry C')),
+              ),
+              Container(
+                height: 150,
+                color: Colors.amber[300],
+                child: const Center(child: Text('Entry D')),
+              ),
+              Container(
+                height: 150,
+                color: Colors.amber[200],
+                child: const Center(child: Text('Entry E')),
+              ),
+            ],
+          ))
+        ],
+      ),
       extendBody: true,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.cyan.shade400,
