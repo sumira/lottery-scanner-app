5cd4f0c67d2ba7d93a454fda0d72710edfd14992
Sumira <sumiragp@gmail.com>
2024-07-04 17:00:11 +0530

    search bar added to the homepage

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -9,40 +9,229 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _selectedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(title: const Text('lottery'), centerTitle: true),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        child: Text(
-          'Using google fonts',
-          style: GoogleFonts.lato(),
+      appBar: AppBar(
+        toolbarHeight: 160,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
         ),
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20))),
+        backgroundColor: Colors.blue[300],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        currentIndex: _selectedIndex,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.call),
-            label: 'call',
+      body: Column(
+        children: <Widget>[
+          // const SizedBox(
+          //   height: 10,
+          // ),
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: [
+          //     ClipRRect(
+          //       borderRadius: const BorderRadius.only(
+          //           bottomLeft: Radius.circular(30),
+          //           bottomRight: Radius.circular(30)),
+          //       child: Container(
+          //         alignment: Alignment.center,
+          //         constraints: BoxConstraints(
+          //           maxWidth: screenWidth,
+          //           maxHeight: 200,
+          //         ),
+          //         color: Colors.blue[300],
+          //         child: const Text('ClipRRect'),
+          //       ),
+          //     ),
+          //   ],
+          // ),
+
+          const Padding(
+            padding: EdgeInsets.only(top: 8, left: 6, right: 6),
+            child: TextField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: "Search Lotteries Here",
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green))),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            label: 'camera',
+
+          SizedBox(
+            height: 100,
+            child: ListView(
+              padding: const EdgeInsets.all(4.0),
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Container(
+                      padding: const EdgeInsets.only(right: 10),
+                      width: 100,
+                      color: Colors.red[600],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Container(
+                      padding: const EdgeInsets.only(right: 10),
+                      width: 100,
+                      color: Colors.red[500],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Container(
+                      width: 100,
+                      color: Colors.red[400],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Container(
+                      width: 100,
+                      color: Colors.red[300],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Container(
+                      width: 100,
+                      color: Colors.red[200],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Container(
+                      width: 100,
+                      color: Colors.red[100],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'chat',
+          const SizedBox(
+            height: 5,
           ),
+
+          Expanded(
+              child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 4.0),
+                child: Container(
+                  height: 150,
+                  color: Colors.amber[600],
+                  child: const Center(child: Text('Entry A')),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 4.0),
+                child: Container(
+                  height: 150,
+                  color: Colors.amber[500],
+                  child: const Center(child: Text('Entry B')),
+                ),
+              ),
+              Container(
+                height: 150,
+                color: Colors.amber[400],
+                child: const Center(child: Text('Entry C')),
+              ),
+              Container(
+                height: 150,
+                color: Colors.amber[300],
+                child: const Center(child: Text('Entry D')),
+              ),
+              Container(
+                height: 150,
+                color: Colors.amber[200],
+                child: const Center(child: Text('Entry E')),
+              ),
+            ],
+          ))
         ],
+      ),
+      //extendBody: true,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue[300],
+        tooltip: 'Increment',
+        onPressed: () {},
+        shape: const CircleBorder(),
+        child: const Icon(Icons.camera, size: 30),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        height: 60,
+        color: Colors.blue[300],
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 8,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.notification_important,
+                color: Colors.black,
+                size: 30,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.star,
+                color: Colors.black,
+                size: 30,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.lock_clock,
+                color: Colors.black,
+                size: 30,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.settings,
+                color: Colors.black,
+                size: 30,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
