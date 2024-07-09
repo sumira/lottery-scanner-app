2ab5909879021a127fdfc85c823c4ae13445d2b7
Sumira <sumiragp@gmail.com>
2024-07-10 04:15:25 +0530

    created a dynamic list and its data model

---
+++ lib/data_models/ticket.dart
@@ -0,0 +1,22 @@
+class Ticket {
+  final String name;
+  final String drawNumber;
+  final String winningNumbers;
+
+  Ticket(
+    this.name,
+    this.drawNumber,
+    this.winningNumbers,
+  );
+}
+
+List tickets = [
+  Ticket('Mahajana Sampatha', '', ''),
+  Ticket('Lagna Wasanawa', '', ''),
+  Ticket('Neeroga', '', ''),
+  Ticket('Dana Nidanaya', '', ''),
+  Ticket('Govisetha', '', ''),
+  Ticket('Mahajana Sampatha 1', '', ''),
+  Ticket('Mahajana Sampatha 2', '', ''),
+  Ticket('Mahajana Sampatha 3', '', ''),
+];
